d4cc80850a51eec9af69a5c8e65782c378218625
Aditya Bhasu Piya <aditya.b.piya@gmail.com>
2019-06-18 03:51:54 +0000

    made changes to schema and added step 6 to show

--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,14 @@ ActiveRecord::Schema.define(version: 2019_06_18_021051) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text     "caption"
+    t.integer  "place_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
+    t.string   "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
